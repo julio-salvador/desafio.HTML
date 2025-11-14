e846451e8f8fc9fdca96faf83b8d07fb836d4388
julio-salvador <julio.girardi@escola.pr.gov.br>
2025-11-14 20:45:33 +0000

    .

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 
+     
+function acender() {
+        document.getElementById("lampada").src = "lampada.png";
+        document.getElementById("imagemExtra").src = "deboa.jpg";
+ }
+
+ function apagar() {
+        document.getElementById("lampada").src = "lampadaapagada.png";
+        document.getElementById("imagemExtra").src = "nodeboa.jpg";
+ }
+
+ function eclipse() {
+         document.getElementById("lampada").src = "eclipse.png";
+         document.getElementById("imagemExtra").src = "assustado.jpg";
+ }
+
+function churrasco() {
+        document.getElementById("lampada").src = "churrasco.png";
+        document.getElementById("imagemExtra").src = "feliz.jpg";
+ }
 
 
 
-function acender(){
-    document.getElementById("lampada").src="lampada.png"
-    console.log("ACENDER")
-}
-
-
-
+     
 
 
 
